7c6c1a948375e05d133b0bd94613bd3b2966ef7e
tom couperus <tcouperus@hotmail.nl>
2023-09-20 11:21:54 +0200

    Remove namespaces from import

--- Unity/Assets/Space/CelestialBody.cs
+++ Unity/Assets/Space/CelestialBody.cs
@@ -3,105 +3,111 @@ using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 
-namespace TCoup.Toolbox.Space {
-    [RequireComponent(typeof(Rigidbody))]
-    public class CelestialBody : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class CelestialBody : MonoBehaviour
+{
 
-        Rigidbody rb;
+    Rigidbody rb;
 
-        [Header("Orbit")]
-        [SerializeField]
-        CelestialBody orbitFocus;
-        [SerializeField]
-        float apoapsis;
-        [SerializeField]
-        float periapsis;
-        [SerializeField]
-        [Tooltip("// TODO")]
-        float eccentricity;
+    [Header("Orbit")]
+    [SerializeField]
+    CelestialBody orbitFocus;
+    [SerializeField]
+    float apoapsis;
+    [SerializeField]
+    float periapsis;
+    [SerializeField]
+    [Tooltip("// TODO")]
+    float eccentricity;
 
-        float semiMajorAxis { get => (apoapsis + periapsis) / 2; }
-        float semiMinorAxis { get => semiMajorAxis * Mathf.Sqrt(1 - eccentricity * eccentricity); }
+    float semiMajorAxis { get => (apoapsis + periapsis) / 2; }
+    float semiMinorAxis { get => semiMajorAxis * Mathf.Sqrt(1 - eccentricity * eccentricity); }
 
-        [SerializeField]
-        [Range(0, 180)]
-        [Tooltip("// TODO")]
-        float inclination;
+    [SerializeField]
+    [Range(0, 180)]
+    [Tooltip("// TODO")]
+    float inclination;
 
-        [SerializeField]
-        [Range(-180, 180)]
-        [Tooltip("Place where the orbit ascends through its plane of reference, with respect to the positive x-axis.")]
-        float longitudeAscendingNode;
+    [SerializeField]
+    [Range(-180, 180)]
+    [Tooltip("Place where the orbit ascends through its plane of reference, with respect to the positive x-axis.")]
+    float longitudeAscendingNode;
 
-        [Header("Planet")]
-        [SerializeField]
-        private float mass;
-        [SerializeField]
-        private float radius;
+    [Header("Planet")]
+    [SerializeField]
+    private float mass;
+    [SerializeField]
+    private float radius;
 
-        [Header("Simulation")]
-        public bool isStationary;
+    [Header("Simulation")]
+    public bool isStationary;
 
-        void Awake() {
-            rb = GetComponent<Rigidbody>();
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Sets the initial velocity for the CelestialObject (m1) relative to all other objects (m2) in the star system, 
+    /// by using the formula:
+    /// v = sqrt(G * m2 / r)
+    /// </summary>
+    public void SetInitialVelocity()
+    {
+        foreach (CelestialBody other in SpaceSimulation.current.celestialBodies)
+        {
+            if (this == other) continue;
+
+            float m2 = other.rb.mass;
+            float r = Vector3.Distance(transform.position, other.transform.position);
+
+            float magnitudeV = Mathf.Sqrt(SpaceSimulation.G * m2 / r);
+            Vector3 directionV = (other.transform.position - transform.position).normalized;
+            directionV = Quaternion.AngleAxis(90, transform.up) * directionV;
+            Vector3 velocity = magnitudeV * directionV;
+
+            rb.velocity += velocity;
         }
+    }
 
-        /// <summary>
-        /// Sets the initial velocity for the CelestialObject (m1) relative to all other objects (m2) in the star system, 
-        /// by using the formula:
-        /// v = sqrt(G * m2 / r)
-        /// </summary>
-        public void SetInitialVelocity() {
-            foreach (CelestialBody other in SpaceSimulation.current.celestialBodies) {
-                if (this == other) continue;
+    /// <summary>
+    /// Applies the standard physics formula for gravity:
+    /// Fg = G * m1 * m2 / r^2
+    /// to each CelestialObject in the system
+    /// </summary>
+    public void ApplyGravity()
+    {
+        foreach (CelestialBody other in SpaceSimulation.current.celestialBodies)
+        {
+            if (this == other) continue;
 
-                float m2 = other.rb.mass;
-                float r = Vector3.Distance(transform.position, other.transform.position);
+            float m1 = rb.mass;
+            float m2 = other.rb.mass;
+            float sqrDistance = (other.transform.position - transform.position).sqrMagnitude;
 
-                float magnitudeV = Mathf.Sqrt(SpaceSimulation.G * m2 / r);
-                Vector3 directionV = (other.transform.position - transform.position).normalized;
-                directionV = Quaternion.AngleAxis(90, transform.up) * directionV;
-                Vector3 velocity = magnitudeV * directionV;
+            Vector3 directionFg = (other.transform.position - transform.position).normalized;
+            float magnitudeFg = SpaceSimulation.G * m1 * m2 / sqrDistance;
+            Vector3 Fg = magnitudeFg * directionFg;
 
-                rb.velocity += velocity;
-            }
+            rb.AddForce(Fg);
         }
+    }
 
-        /// <summary>
-        /// Applies the standard physics formula for gravity:
-        /// Fg = G * m1 * m2 / r^2
-        /// to each CelestialObject in the system
-        /// </summary>
-        public void ApplyGravity() {
-            foreach (CelestialBody other in SpaceSimulation.current.celestialBodies) {
-                if (this == other) continue;
-
-                float m1 = rb.mass;
-                float m2 = other.rb.mass;
-                float sqrDistance = (other.transform.position - transform.position).sqrMagnitude;
-
-                Vector3 directionFg = (other.transform.position - transform.position).normalized;
-                float magnitudeFg = SpaceSimulation.G * m1 * m2 / sqrDistance;
-                Vector3 Fg = magnitudeFg * directionFg;
-
-                rb.AddForce(Fg);
-            }
-        }
-
-        public void SetInitialPositionCircular() {
-            if (orbitFocus == null) return;
-            float loanRad = longitudeAscendingNode * Mathf.Deg2Rad;
-            float x = Mathf.Cos(loanRad) * periapsis;
-            float z = Mathf.Sin(loanRad) * periapsis;
-            transform.position = orbitFocus.transform.position + new Vector3(x, 0, z);
-        }
+    public void SetInitialPositionCircular()
+    {
+        if (orbitFocus == null) return;
+        float loanRad = longitudeAscendingNode * Mathf.Deg2Rad;
+        float x = Mathf.Cos(loanRad) * periapsis;
+        float z = Mathf.Sin(loanRad) * periapsis;
+        transform.position = orbitFocus.transform.position + new Vector3(x, 0, z);
+    }
 
 #if UNITY_EDITOR
-        private void OnValidate() {
-            if (rb != null) rb.mass = mass;
-            transform.localScale = Vector3.one * radius;
-            SetInitialPositionCircular();
-        }
-#endif
+    private void OnValidate()
+    {
+        if (rb != null) rb.mass = mass;
+        transform.localScale = Vector3.one * radius;
+        SetInitialPositionCircular();
     }
+#endif
 }
--- Unity/Assets/Space/SpaceSimulation.cs
+++ Unity/Assets/Space/SpaceSimulation.cs
@@ -2,45 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TCoup.Toolbox.Space {
-    [RequireComponent(typeof(TimeController))]
-    public class SpaceSimulation : MonoBehaviour {
-        public static SpaceSimulation current;
-        public static float G = 100f;
+[RequireComponent(typeof(TimeController))]
+public class SpaceSimulation : MonoBehaviour
+{
+    public static SpaceSimulation current;
+    public static float G = 100f;
 
-        public bool isInitialized { get; private set; }
-        public CelestialBody[] celestialBodies;
+    public bool isInitialized { get; private set; }
+    public CelestialBody[] celestialBodies;
 
-        private void Awake() {
-            if (current != null) {
-                Destroy(this);
-                return;
-            }
-            current = this;
-            celestialBodies = FindObjectsOfType<CelestialBody>();
-            isInitialized = false;
+    private void Awake()
+    {
+        if (current != null)
+        {
+            Destroy(this);
+            return;
         }
+        current = this;
+        celestialBodies = FindObjectsOfType<CelestialBody>();
+        isInitialized = false;
+    }
 
-        private void Start() {
-            Debug.Log("Initializing");
-            TimeController.current.Pause();
-            foreach (CelestialBody cb in celestialBodies) {
-                cb.SetInitialPositionCircular();
-            }
-            foreach (CelestialBody cb in celestialBodies) {
-                if (cb.isStationary) continue;
-                cb.SetInitialVelocity();
-            }
-            isInitialized = true;
-            Debug.Log("Initialized Space Simulation");
+    private void Start()
+    {
+        Debug.Log("Initializing");
+        TimeController.current.Pause();
+        foreach (CelestialBody cb in celestialBodies)
+        {
+            cb.SetInitialPositionCircular();
         }
+        foreach (CelestialBody cb in celestialBodies)
+        {
+            if (cb.isStationary) continue;
+            cb.SetInitialVelocity();
+        }
+        isInitialized = true;
+        Debug.Log("Initialized Space Simulation");
+    }
 
-        private void FixedUpdate() {
-            if (!isInitialized) return;
-            foreach (CelestialBody cb in celestialBodies) {
-                if (cb.isStationary) continue;
-                cb.ApplyGravity();
-            }
+    private void FixedUpdate()
+    {
+        if (!isInitialized) return;
+        foreach (CelestialBody cb in celestialBodies)
+        {
+            if (cb.isStationary) continue;
+            cb.ApplyGravity();
         }
     }
 }
